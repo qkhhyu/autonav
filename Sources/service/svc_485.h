0c0b6270e39ed73c5951880c5d96023ad55f5887
qkhhyu <qkhhyu@163.com>
2020-12-23 17:12:02 +0800

    main

--- Sources/service/svc_485.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SVC_485_H
-#define __SVC_485_H
-
-void svc_485_init(void);
-
-#endif
--- Sources/service/svc_dist.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SVC_DIST_H
-#define __SVC_DIST_H
-
-void svc_dist_init(void);
-void svc_dist_bind(void(*handler)(struct dist *dist));
-void svc_dist_start(void);
-void svc_dist_stop(void);
-
-#endif
